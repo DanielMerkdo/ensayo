708262a51bda4f63d1bd7af77a3ba61aa52347a9
jmsuarezmon <javier.suarezm@udea.edu.co>
2025-05-07 18:49:13 -0500

    Actualizacion preprocesamiento sql

--- preprocesamiento.sql
+++ preprocesamiento.sql
@@ -17,10 +17,51 @@ drop table if exists movies_sel;
 
 
 create table movies_sel as select movieId,
-                         count(*) as cnt_rat
-                         from ratings
-                         group by movieId
-                         having cnt_rat >5
-                         order by cnt_rat desc ;
+    count(*) as cnt_rat
+    from ratings
+    group by movieId
+    having cnt_rat >5
+    order by cnt_rat desc ;
                          
-------crear tablas filtradas de libros, usuarios y calificaciones ----
+------crear tablas filtradas de  peliculas, usuarios y calificaciones ----
+
+
+drop table if exists ratings_final;
+
+create table ratings_final as
+select a."userId"as user_id,
+a.movieId as movie_Id,
+a."rating" as movie_rating,
+a."timestamp" as rating_time
+from ratings a 
+inner join movies_sel b
+on a.movieId =b.movieId
+inner join usuarios_sel c
+on a."userId" =c.user_id;
+
+
+
+-- VERIFICAR CODIGO ABAJO
+
+drop table if exists movies_final;
+
+create table movies_final as
+select a.movieId as movie_id,
+a."title"  as movie_title,
+a."genres" as movie_genre
+from movies a
+inner join movies_sel c
+on a.movieId= c.movieId;
+
+
+
+---crear tabla completa ----
+
+drop table if exists full_ratings ;
+
+create table full_ratings as select 
+a.*,
+c.movie_title,
+c.movie_genre
+from ratings_final a inner join
+movies_final c on a.movie_Id=c.movie_Id;
